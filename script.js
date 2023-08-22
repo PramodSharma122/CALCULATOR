d7046ae467c8511838d32e8e75f718eaf35efb98
PramodSharma122 <pramodchalise122@gmail.com>
2023-08-22 21:30:03 +0545

    first commit

---
+++ script.js
@@ -0,0 +1,23 @@
+let string = "";
+let input = document.getElementById('inputBox');
+let button = document.querySelectorAll('button');
+Array.from(button).forEach((button)=>{
+    button.addEventListener('click',(e)=>{
+        if(e.target.innerHTML == '='){
+            string = eval(string);
+            document.querySelector('input').value = string;
+        }
+        else if(e.target.innerHTML == 'AC'){
+            string = ""
+            document.querySelector('input').value = string;
+        }
+        else if(e.target.innerHTML == 'DEL'){
+            string = string.substring(0, string.length-1);
+            document.querySelector('input').value = string;
+        }
+        else {
+            string = string + e.target.innerHTML;
+            document.querySelector('input').value = string;
+        }
+    })
+})
